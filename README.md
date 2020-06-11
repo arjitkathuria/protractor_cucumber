1e876fdb35dbe5bda587cab370a0b624e930d632
Arjit Kathuria <arjit.kathuria@nagarro.com>
2020-06-11 16:09:53 +0530

    added all files

---
+++ README.md
@@ -0,0 +1,89 @@
+# NAGP ASSIGNMENT PROTRACTOR
+
+# Wow Factor
+
+* Haven't used browser.sleep in the framework, Used only async and await functions.
+
+* Each and every test case is isolated, Just need to mention the tag name under cucumberOpts of config.js. 
+
+tags: "@loginLogoutTestCase"
+
+Also created different scripts under package.json for all type of test cases.
+
+* Almost each and every step defination is generic so it can used for multiple scenario.
+
+* Zip the previous report as per the current timestamp
+
+* Taking screenshot of failed scenario with their scenario name under output folder as well as attaching the screenshot in cucmber test report.
+
+* Under Utility, I have created some generic functions which is used in framework at each level. That has been reduced the code in stepdefinations and their pages.
+
+Everything has been controlled only through commonFunctions.js
+
+* Created seperate property file to read the general information of the test like URL of the application.
+
+* Used proper coding practice, Like using of let and camel casing
+
+* Added Negative scenario for withdrawing more money than balance
+
+**What all dependencies used for this task?**
+-----
+ * Chai
+ * Cucumber
+ * log4js
+ * log4js-protractor-appender
+ * protractor
+ * protractor-cucumber-framework
+ * protractor-multiple-cucumber-html-reporter-plugin
+ * Gherkin
+ * Java Script
+ 
+
+**What all I have included in this project?**
+----
+*PROTRACTORASSIGNMENT.features folder* : It contains all of the test scenario is in plain english.
+
+*PROTRACTORASSIGNMENT.hooks folder* : Contains all of the steps which needs to be executed before and after the execution of test case.
+
+*PROTRACTORASSIGNMENT.logs folder* : Contains logs is in text file for the last execution of the framework.
+
+*PROTRACTORASSIGNMENT.objectRepository folder* : Contains all of the information about the webelements and I have segreated this information at page level. Each page has their own object repoistory.
+
+*PROTRACTORASSIGNMENT.pages folder* : Contains all of the methods corresponds to their page.
+
+*PROTRACTORASSIGNMENT.stepDefinations folder* : Contains all of the step definitions corresponds to their feature files.
+
+*PROTRACTORASSIGNMENT.testData folder* : Contains all of the test data in JSON format
+
+*PROTRACTORASSIGNMENT.utility folder* : Conatins utils which can be used anywhere in project like camel casing of the string.
+
+# TEST REPORT
+
+Implemented protractor-multiple-cucumber-html-reporter-plugin test report.
+
+HTML report with is placed inside the .reports//report folder with name index.html.
+
+All the reports archived to individual zip folder as per the timestamp
+
+
+# What all setup required to run the test cases
+
+Just download the project and run few commands in specific order:
+npm install
+npm start
+
+* Created different scripts uunder package.json like:
+    "all": "protractor --cucumberOpts.tags=\"@functionalTestCase\" config.js",
+    "bankManagerLogin": "protractor --cucumberOpts.tags=\"@bankManagerLogin\" config.js",
+    "customerLogin": "protractor --cucumberOpts.tags=\"@customerLogin\" config.js",
+    "generalScenario": "protractor --cucumberOpts.tags=\"@generalScenario\" config.js"
+    "deletecustomers": "protractor --cucumberOpts.tags=\"@deletecustomers\" config.js"
+
+Run:
+npm run all: to execute all of the test
+npm run bankManagerLogin: to execute all of the test related to bank manager login
+npm run customerLogin: to execute all of the test related to customer login
+npm run generalScenario: to execute all of the general test
+
+** Can run through cmd or any editor like visual studio **
+
---
+++ utility/fileUtil.js
@@ -0,0 +1,130 @@
+var fs = require('fs');
+var dateTime = require('node-datetime');
+const {
+    zip
+} = require('zip-a-folder');
+
+var util = function () {
+    this.zip = async function (source) {
+        var destination = await source + '.zip'
+        try {
+            await zip(source, destination);
+        } catch (e) {
+            await commonLogger.error(e);
+            throw e;
+        }
+    };
+
+    this.copyFile = async function (source, destination) {
+        try {
+            await fs.copyFile(source, destination, async (err) => {
+                if (err) {
+                    await commonLogger.error(err);
+                    throw err;
+                }
+            });
+        } catch (e) {
+            await commonLogger.error(e);
+            throw e;
+        }
+    };
+
+    this.archiveReport = async function (report) {
+
+        try {
+            var temp = await report.split("/");
+            var currentDateTime = await this.getCurrentDateTime();
+            var dateFormat = await currentDateTime.replaceAll(" ", "_");
+            var dateFormatModify = await dateFormat.replaceAll(":", "_");
+            dateFormatModify = await dateFormatModify.replaceAll("-", "_");
+            var arcFolder = await report.replace(temp[temp.length - 1], dateFormatModify);
+            await this.createFolder(arcFolder);
+            var destination = await arcFolder + "/" + temp[temp.length - 1];
+            await this.copyFile(report, destination);
+            await this.zip(arcFolder);
+            await this.deleteFolder(arcFolder);
+            commonLogger.info("Archived Folder : " + arcFolder);
+        } catch (e) {
+            await commonLogger.error(e);
+            throw e;
+        }
+    }
+
+    String.prototype.replaceAll = async function (search, replacement) {
+        try {
+            var target = this;
+            return await target.replace(new RegExp(search, 'g'), replacement);
+        } catch (e) {
+            await commonLogger.error(e);
+            throw e;
+        }
+    };
+
+    this.getCurrentDateTime = async function () {
+        try {
+            var dt = await dateTime.create();
+            return await dt.format('Y-m-d H:M:S');
+        } catch (e) {
+            await commonLogger.error(e);
+            throw e;
+        }
+    };
+
+    this.createFolder = async function (folder) {
+        try {
+            if (await !fs.existsSync(folder)) {
+                await fs.mkdirSync(folder);
+            }
+        } catch (e) {
+            await commonLogger.error(e);
+            throw e;
+        }
+    }
+
+    this.deleteFolder = async function (path) {
+
+        try {
+            if (await fs.existsSync(path)) {
+                await fs.readdirSync(path).forEach(async function (file, index) {
+                    var curPath = path + "/" + file;
+                    if (await fs.lstatSync(curPath).isDirectory()) { 
+                        await deleteFolder(curPath);
+                    } else { 
+                        await fs.unlinkSync(curPath);
+                    }
+                });
+                await fs.rmdirSync(path);
+            }
+        } catch (e) {
+            await commonLogger.error(e);
+            throw e;
+        }
+    };
+
+    this.createReportFolder = async function () {
+        try {
+            var currentDateTime = await this.getCurrentDateTime();
+            var dateFormat = await currentDateTime.replaceAll(" ", "_"); 
+            var dateFormatModify = await dateFormat.replaceAll(":", "_"); 
+            dateFormatModify = await dateFormatModify.replaceAll("-", "_");
+            var arcFolder = "./reports/";
+            await this.createFolder(arcFolder);
+            await this.createFolder(arcFolder + dateFormatModify);
+        } catch (e) {
+            await commonLogger.error(e);
+            throw e;
+        }
+    }
+
+    this.createBlankJson = function (file) {
+        // appendFile function with filename, content and callback function
+        fs.appendFile(file, '', function (err) {
+            if (e) throw e;
+            commonLogger.log('File is created successfully.');
+        });
+    }
+};
+
+module.exports = new util();
+
+
